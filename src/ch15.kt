3be8c527ec6f93249c259381ad43deabdb93cda2
Kushagra23d <kushagra.23.cse@gmail.com>
2021-11-21 21:20:48 +0530

    cc

--- src/ch15.kt
+++ src/ch15.kt
@@ -7,3 +7,56 @@
  public means that any client who sees the declaring class sees its public members.
  ***/
 
+
+open class Father6{
+ // Properties
+ var a:Int = 10
+ var b:Int = 20
+ var c:Int = 30
+ var d:Int = 40
+
+ // Member Function
+ fun disp(){
+  println("************************")
+
+  println("A:$a")
+  println("B:$b")
+  println("C:$c")
+  println("D:$d")
+ }
+
+ fun hello(){
+  println("Hello Father")
+ }
+
+}
+
+class Son6:Father6(){
+ //Properties
+ var bike:String = "K 10"
+
+ // Member Function
+
+ fun show(){
+  println("************************")
+
+  println("A:$a")
+  println("B:$b")
+  println("C:$c")
+  println("D:$d")
+  hello()
+ }
+}
+
+fun main(){
+ val s = Son6()
+ s.show()
+ s.disp()
+ s.a = 101
+ s.b = 102
+ s.c = 103
+ s.d = 104
+ s.show()
+ s.hello()
+
+}
