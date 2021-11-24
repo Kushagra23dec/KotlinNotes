1f223c89acf660aaffe930c8670102845b83aefd
Kushagra23d <kushagra.23.cse@gmail.com>
2021-11-24 14:52:40 +0530

    cc

---
+++ src/ch16.kt
@@ -0,0 +1,39 @@
+/******** Abstract class and Abstract Method *************/
+
+abstract class Father7{         // Abstract class are always open, but we cann't create it's object
+    // Properties
+    var car:String = "Alt 100"
+
+    //Member function
+    fun disp(){
+        println(car)
+    }
+
+    // Abstract Method
+
+    abstract fun hello()
+}
+
+
+class  Son7:Father7(){
+    //Member function
+    fun show(){
+        println("Father car: $car")
+    }
+
+    override
+    fun hello(){
+        println("Hello for Hello function.")
+    }
+
+
+}
+
+fun main(){
+    val s1 = Son7()
+    s1.show()
+    s1.disp()
+    s1.hello()
+//    val f1 = Father7()         // cann't create object/instance of abstract class
+
+}
